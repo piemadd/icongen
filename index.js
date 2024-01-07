b38974791449cadd8627c194b3fbe4477885a691
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2024-01-06 23:14:56 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+//removing old icons
+if (fs.existsSync('./icons')) fs.rmSync('./icons', { recursive: true });
+fs.mkdirSync('./icons');
+
+let allColorSets = [];
+const inputFiles = fs.readdirSync('./inputs');
+
+for (let i = 0; i < inputFiles.length; i++) {
+  const inputFile = inputFiles[i];
+
+  const inputFileContent = JSON.parse(fs.readFileSync(`./inputs/${inputFile}`, { encoding: 'utf8' }));
+
+  allColorSets.push(...inputFileContent)
+}
+
+allColorSets = [...new Set(allColorSets.map(n => n.toUpperCase()).sort())]
+
+//template svg
+const iconTemplate = fs.readFileSync('./template.svg', 'utf8');
+
+allColorSets.forEach((colorSet) => {
+  const iconFilled = iconTemplate.replaceAll("#FFFFFF", `#${colorSet.split('_')[0]}`).replaceAll("#000000", colorSet.split('_')[1]);
+  const iconBuffer = Buffer.from(iconFilled, 'utf8');
+
+  sharp(iconBuffer)
+    .resize(120, 120)
+    .png()
+    .toFile(`./icons/${colorSet}.png`, (err, info) => {
+      if (err) throw err;
+    });
+})
